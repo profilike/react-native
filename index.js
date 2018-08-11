d570da5e8456f9f295f4872f211b73e1dc9dc3be
Vasyl <vasylpb89@gmail.com>
2018-08-11 21:05:51 +0300

    Redux, add react-native-vector-icons

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './src/store/configureStore';
 import App from './App';
-AppRegistry.registerComponent('rncource', () => App);
+
+const store = configureStore();
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent('rncource', () => RNRedux);
