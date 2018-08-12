ba7430b8d2119011cda747752e1837f54ff622de
Vasyl <vasylpb89@gmail.com>
2018-08-12 21:52:38 +0300

    Tab navigation, Drawer

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import { Navigation } from 'react-native-navigation';
-
+import { Provider } from 'react-redux';
+import configureStore from './src/store/configureStore';
 import AuthScreen from './src/screens/Auth/Auth';
+import SharePlace from './src/screens/SharePlace/SharePlace';
+import FindPlace from './src/screens/FindPlace/FindPlace';
+import PlaceDetail from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 
-// register Screens
-Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
+const store = configureStore();
+
+// Register Screens
+Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen, store, Provider);
+Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlace, store, Provider);
+Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlace, store, Provider);
+Navigation.registerComponent('awesome-places.PlaceDetailScreen', () => PlaceDetail, store, Provider);
+Navigation.registerComponent('awesome-places.SideDrawer', () => SideDrawer);
 
 // Start a App
 Navigation.startSingleScreenApp({
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,8 +1,6 @@
 import { 
   ADD_PLACE,
-  DELETE_PLACE,
-  SELECT_PLACE,
-  DESELECT_PLACE
+  DELETE_PLACE
 } from './actionTypes';
 
 export const addPlace = placeName => {
@@ -11,19 +9,9 @@ export const addPlace = placeName => {
     placeName
   };
 };
-export const deletePlace = () => {
+export const deletePlace = key => {
   return {
-    type: DELETE_PLACE
-  };
-};
-export const selectPlace = key => {
-  return {
-    type: SELECT_PLACE,
+    type: DELETE_PLACE,
     placeKey: key
-  };
-};
-export const deselectPlace = () => {
-  return {
-    type: DESELECT_PLACE
   };
 };
